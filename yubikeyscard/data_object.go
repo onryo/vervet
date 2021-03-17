a193e3b103ce57c85d308e4f1498249b6feaef3e
George Watkins <g@watkins.io>
2021-03-17 00:30:05 -0700

    implemented dataObjects retrieval and deserialization

--- yubikeyscard/data_object.go
+++ yubikeyscard/data_object.go
@@ -1,8 +1,8 @@
 package yubikeyscard
 
 import (
+	"bytes"
 	"encoding/binary"
-	"errors"
 )
 
 const (
@@ -25,13 +25,13 @@ var doName = DataObject{tag: 0x005B, constructed: false, parent: 0x65, binary: f
 var doLangPrefs = DataObject{tag: 0x5F2D, constructed: false, parent: 0x65, binary: false, extLen: 0, desc: "Language preferences"}
 var doAppRelData = DataObject{tag: 0x006E, constructed: true, parent: 0, binary: true, extLen: 0, desc: "Application Related Data"}
 var doLoginData = DataObject{tag: 0x005E, constructed: false, parent: 0, binary: true, extLen: 2, desc: "Login Data"}
-var doAppID = DataObject{tag: 0x004F, constructed: false, parent: 0x6E, binary: true, extLen: 0, desc: "Application Idenfifier (AID)"}
+var doAID = DataObject{tag: 0x004F, constructed: false, parent: 0x6E, binary: true, extLen: 0, desc: "Application Idenfifier (AID)"}
 var doDiscrDOs = DataObject{tag: 0x0073, constructed: true, parent: 0, binary: true, extLen: 0, desc: "Discretionary Data Objects"}
 var doCardCaps = DataObject{tag: 0x0047, constructed: false, parent: 0x6E, binary: true, extLen: 0, desc: "Card Capabilities"}
 var doExtLenCaps = DataObject{tag: 0x00C0, constructed: false, parent: 0x6E, binary: true, extLen: 0, desc: "Extended Card Capabilities"}
-var doAlgoAttrSig = DataObject{tag: 0x00C1, constructed: false, parent: 0x6E, binary: true, extLen: 0, desc: "Algorithm Attributes Signature"}
-var doAlgoAttrDec = DataObject{tag: 0x00C2, constructed: false, parent: 0x6E, binary: true, extLen: 0, desc: "Algorithm Attributes Decryption"}
-var doAlgoAttrAut = DataObject{tag: 0x00C3, constructed: false, parent: 0x6E, binary: true, extLen: 0, desc: "Algorithm Attributes Authentication"}
+var doAlgoAttrSign = DataObject{tag: 0x00C1, constructed: false, parent: 0x6E, binary: true, extLen: 0, desc: "Algorithm Attributes Signature"}
+var doAlgoAttrEnc = DataObject{tag: 0x00C2, constructed: false, parent: 0x6E, binary: true, extLen: 0, desc: "Algorithm Attributes Encryption"}
+var doAlgoAttrAuth = DataObject{tag: 0x00C3, constructed: false, parent: 0x6E, binary: true, extLen: 0, desc: "Algorithm Attributes Authentication"}
 var doPWStatus = DataObject{tag: 0x00C4, constructed: false, parent: 0x6E, binary: true, extLen: 0, desc: "Password Status Bytes"}
 var doFingerprints = DataObject{tag: 0x00C5, constructed: false, parent: 0x6E, binary: true, extLen: 0, desc: "Fingerprints"}
 var doCAFingerprints = DataObject{tag: 0x00C6, constructed: false, parent: 0x6E, binary: true, extLen: 0, desc: "CA Fingerprints"}
@@ -56,8 +56,8 @@ var doAlgoInfo = DataObject{tag: 0x00FA, constructed: false, parent: 0, binary: 
 
 var DataObjects = []DataObject{
 	doURL, doHistBytes, doCardRelData, doName, doLangPrefs, doAppRelData,
-	doLoginData, doAppID, doDiscrDOs, doCardCaps, doExtLenCaps, doAlgoAttrSig,
-	doAlgoAttrDec, doAlgoAttrAut, doPWStatus, doFingerprints, doCAFingerprints,
+	doLoginData, doAID, doDiscrDOs, doCardCaps, doExtLenCaps, doAlgoAttrSign,
+	doAlgoAttrEnc, doAlgoAttrAuth, doPWStatus, doFingerprints, doCAFingerprints,
 	doKeyGenDate, doSecSuppTmpl, doDigSigCtr, doPrivateDO1, doPrivateDO2,
 	doPrivateDO3, doPrivateDO4, doCardholderCrt, doGenFeatMgmt, doAESKeyData,
 	doUIFSig, doUIFDec, doUIFAut, doUIFAtt, doKDFDO, doAlgoInfo,
@@ -77,7 +77,26 @@ func (do DataObject) tagP2() byte {
 	return do.tagBytes()[1]
 }
 
-func doFindTLV(data []byte, tag uint16, nestLevel int) ([]byte, error) {
+func (do DataObject) parentBytes() []byte {
+	b := make([]byte, 2)
+	binary.BigEndian.PutUint16(b, uint16(do.parent))
+	return b
+}
+
+func (do DataObject) getChildren() []DataObject {
+	var c []DataObject
+
+	for _, d := range DataObjects {
+		if bytes.Compare(d.parentBytes(), do.tagBytes()) == 0 {
+			c = append(c, d)
+		}
+	}
+
+	return c
+}
+
+// doFindTLV function is based on GnuPG implementation of do_find_tlv in common/tlv.c
+func doFindTLV(data []byte, tag uint16, nestLevel int) []byte {
 	var o int = 0
 	var n int = len(data)
 	var tagLen uint16
@@ -87,7 +106,7 @@ func doFindTLV(data []byte, tag uint16, nestLevel int) ([]byte, error) {
 
 	for !tagFound {
 		if n < 2 { // Buffer definitely too short for tag and length.
-			return nil, errors.New("No data present in buffer")
+			return nil
 		}
 
 		if data[o] == 0 || data[o] == 0xff { // Skip optional filler between TLV objects.
@@ -102,10 +121,10 @@ func doFindTLV(data []byte, tag uint16, nestLevel int) ([]byte, error) {
 			n--
 
 			if n < 2 { // Buffer definitely too short for tag and length.
-				return nil, errors.New("No data present in buffer")
+				return nil
 			}
 			if (data[o] & 0x1f) == 0x1f { // We support only up to 2 bytes.
-				return nil, errors.New("Supports only up to 2 bytes")
+				return nil
 			}
 
 			thisTag = binary.BigEndian.Uint16([]byte{data[o-1], data[o] & 0x7f})
@@ -120,7 +139,7 @@ func doFindTLV(data []byte, tag uint16, nestLevel int) ([]byte, error) {
 			// do nothing
 		} else if tagLen == 0x81 { // One byte length follows.
 			if n != 0 { // we expected 1 more bytes with the length
-				return nil, errors.New("Expected 1 more bytes with the length")
+				return nil
 			}
 
 			tagLen = binary.BigEndian.Uint16([]byte{0, data[o]})
@@ -128,36 +147,33 @@ func doFindTLV(data []byte, tag uint16, nestLevel int) ([]byte, error) {
 			n--
 		} else if tagLen == 0x82 { // Two byte length follows
 			if n < 2 { // We expected 2 more bytes with the length.
-				return nil, errors.New("Expected 2 more bytes with the length")
+				return nil
 			}
 
 			tagLen = binary.BigEndian.Uint16([]byte{data[o], data[o+1]})
 			o += 2
 			n -= 2
 		} else { // APDU limit is 65535, thus it does not make sense to assume longer length fields. */
-			return nil, errors.New("APDU limit is 65535, thus it does not make sense to assume longer length fields")
+			return nil
 		}
 
 		if composite && nestLevel < 100 { // Dive into this composite DO after checking for a too deep nesting
-			tmpData, err := doFindTLV(data[o:], tag, nestLevel+1)
-			if err != nil {
-				return nil, err
-			}
+			tmpData := doFindTLV(data[o:], tag, nestLevel+1)
 
 			if len(tmpData) > 0 {
-				return tmpData, nil
+				return tmpData
 			}
 		}
 
 		if thisTag == tag {
 			tagFound = true
 		} else if int(tagLen) > n { // Buffer too short to skip to the next tag.
-			return nil, errors.New("Buffer too short to skip to the next tag")
+			return nil
 		} else {
 			o += int(tagLen)
 			n -= int(tagLen)
 		}
 	}
 
-	return data[o : o+int(tagLen)], nil
+	return data[o : o+int(tagLen)]
 }
--- yubikeyscard/yubikeyscard.go
+++ yubikeyscard/yubikeyscard.go
@@ -15,9 +15,14 @@ import (
 // YubiKeys represents the system context and slice of connected smart cards
 
 const (
-	pinMin          int = 6
-	pinMax          int = 127
-	fingerprintSize int = 20
+	pinMin int = 6
+	pinMax int = 127
+)
+
+const (
+	AlgoIdRSA   uint8 = 1
+	AlgoIdECDH  uint8 = 12
+	AlgoIdECDSA uint8 = 13
 )
 
 type YubiKey struct {
@@ -25,10 +30,53 @@ type YubiKey struct {
 	Card    *scard.Card
 }
 
+type AppRelatedData struct {
+	AID          AID
+	AlgoAttrSign AlgoAttr
+	AlgoAttrEnc  AlgoAttr
+	AlgoAttrAuth AlgoAttr
+	PWStatus     PWStatus
+	Fingerprints Fingerprints
+	KeyGenDates  KeyGenDates
+}
+
+type AID struct {
+	RID          [5]byte
+	App          byte
+	Version      [2]byte
+	Manufacturer [2]byte
+	Serial       [4]byte
+	RFU          [2]byte
+}
+
+type AlgoAttr struct {
+	ID              byte
+	RSAModLen       [2]byte
+	RSAPubKeyExpLen [2]byte
+	ECurveOID       []byte
+	PrivKeyImpFmt   byte
+}
+
+type PWStatus struct {
+	PW1Validity   byte
+	PW1MaxLenFmt  byte
+	PW1MaxLenRC   byte
+	PW3MaxLenFmt  byte
+	PW1RetryCtr   byte
+	PW1RCRetryCtr byte
+	PW3RetryCtr   byte
+}
+
 type Fingerprints struct {
-	Sign    []byte
-	Encrypt []byte
-	Auth    []byte
+	Sign [20]byte
+	Enc  [20]byte
+	Auth [20]byte
+}
+
+type KeyGenDates struct {
+	Sign [4]byte
+	Enc  [4]byte
+	Auth [4]byte
 }
 
 var yubikeyReaderID = "Yubico YubiKey OTP+FIDO+CCID"
@@ -197,34 +245,131 @@ func Verify(card *scard.Card, pin []byte) error {
 	return nil
 }
 
-func GetFingerprints(card *scard.Card) (Fingerprints, error) {
-	fps := Fingerprints{}
+func GetAppRelatedData(card *scard.Card) (AppRelatedData, error) {
+	var ard AppRelatedData
 	data, err := GetData(card, doAppRelData)
 	if err != nil {
-		return fps, err
+		return AppRelatedData{}, err
 	}
 
-	fpData, err := doFindTLV(data, doFingerprints.tag, 2)
-	if err != nil {
-		return fps, err
-	}
+	for _, c := range doAppRelData.getChildren() {
+		cData := doFindTLV(data, c.tag, 1)
+		buf := bytes.NewReader(cData)
 
-	buf := bytes.NewReader(fpData)
-	var fpSign, fpEncrypt, fpAuth []byte
-	for _, fp := range []*[]byte{&fpSign, &fpEncrypt, &fpAuth} {
-		*fp = make([]byte, fingerprintSize)
-		if err := binary.Read(buf, binary.BigEndian, fp); err != nil {
-			return fps, err
+		switch c.tag {
+		case doAID.tag:
+			ard.AID, err = readAID(buf)
+		case doAlgoAttrSign.tag:
+			ard.AlgoAttrSign, err = readAlgoAttr(buf)
+		case doAlgoAttrEnc.tag:
+			ard.AlgoAttrEnc, err = readAlgoAttr(buf)
+		case doAlgoAttrAuth.tag:
+			ard.AlgoAttrAuth, err = readAlgoAttr(buf)
+		case doPWStatus.tag:
+			ard.PWStatus, err = readPWStatus(buf)
+		case doFingerprints.tag:
+			ard.Fingerprints, err = readFingerprints(buf)
+		case doKeyGenDate.tag:
+			ard.KeyGenDates, err = readKeyGenDates(buf)
+		}
+
+		if err != nil {
+			return AppRelatedData{}, err
 		}
 	}
 
-	fps = Fingerprints{
-		Sign:    fpSign,
-		Encrypt: fpEncrypt,
-		Auth:    fpAuth,
+	return ard, nil
+}
+
+func readAID(buf *bytes.Reader) (AID, error) {
+	var aid AID
+
+	if err := binary.Read(buf, binary.BigEndian, &aid.RID); err != nil {
+		return AID{}, err
+	}
+	if err := binary.Read(buf, binary.BigEndian, &aid.App); err != nil {
+		return AID{}, err
+	}
+	for _, a := range []*[2]byte{&aid.Version, &aid.Manufacturer} {
+		if err := binary.Read(buf, binary.BigEndian, a); err != nil {
+			return AID{}, err
+		}
+	}
+	if err := binary.Read(buf, binary.BigEndian, &aid.Serial); err != nil {
+		return AID{}, err
+	}
+	if err := binary.Read(buf, binary.BigEndian, &aid.RFU); err != nil {
+		return AID{}, err
+	}
+
+	return aid, nil
+}
+
+func readAlgoAttr(buf *bytes.Reader) (AlgoAttr, error) {
+	var aa AlgoAttr
+
+	if err := binary.Read(buf, binary.BigEndian, &aa.ID); err != nil {
+		return AlgoAttr{}, err
+	}
+
+	switch aa.ID {
+	case AlgoIdRSA:
+		for _, a := range []*[2]byte{&aa.RSAModLen, &aa.RSAPubKeyExpLen} {
+			if err := binary.Read(buf, binary.BigEndian, a); err != nil {
+				return AlgoAttr{}, err
+			}
+		}
+	case AlgoIdECDH, AlgoIdECDSA:
+		aa.ECurveOID = make([]byte, buf.Len()-1)
+		if err := binary.Read(buf, binary.BigEndian, &aa.ECurveOID); err != nil {
+			return AlgoAttr{}, err
+		}
+
+	}
+
+	if err := binary.Read(buf, binary.BigEndian, &aa.PrivKeyImpFmt); err != nil {
+		return AlgoAttr{}, err
+	}
+
+	return aa, nil
+}
+
+func readPWStatus(buf *bytes.Reader) (PWStatus, error) {
+	var pws PWStatus
+	pwb := []*byte{&pws.PW1Validity, &pws.PW1MaxLenFmt, &pws.PW1MaxLenRC,
+		&pws.PW3MaxLenFmt, &pws.PW1RetryCtr, &pws.PW1RCRetryCtr, &pws.PW3RetryCtr}
+
+	for _, p := range pwb {
+		if err := binary.Read(buf, binary.BigEndian, p); err != nil {
+			return PWStatus{}, err
+		}
+	}
+
+	return pws, nil
+}
+
+func readFingerprints(buf *bytes.Reader) (Fingerprints, error) {
+	var fps Fingerprints
+
+	for _, fp := range []*[20]byte{&fps.Sign, &fps.Enc, &fps.Auth} {
+		if err := binary.Read(buf, binary.BigEndian, fp); err != nil {
+			return Fingerprints{}, err
+		}
 	}
 
 	return fps, nil
+}
+
+func readKeyGenDates(buf *bytes.Reader) (KeyGenDates, error) {
+	var kgds KeyGenDates
+
+	for _, kgd := range []*[4]byte{&kgds.Sign, &kgds.Enc, &kgds.Auth} {
+		if err := binary.Read(buf, binary.BigEndian, kgd); err != nil {
+			return KeyGenDates{}, err
+		}
+	}
+
+	return kgds, nil
 }
 
 // Decipher data with private key on smart card
